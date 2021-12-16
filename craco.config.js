14255d95703043b9cfbe794e1f4e1cb2f4966235
赵金柱 <zhaojinzhu@hnshengdun.com>
2021-12-16 15:17:35 +0800

    添加devServer配置

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,8 @@
 /*
  * @Date: 2021-11-18 09:20:51
  * @LastEditors: Aiva
- * @LastEditTime: 2021-12-15 13:45:20
- * @FilePath: \cra-project-template\craco.config.js
+ * @LastEditTime: 2021-12-16 15:13:50
+ * @FilePath: \cra-project-template\craco.config.ts
  */
 const CracoLessPlugin = require("craco-less");
 const themeConfig = require("./theme.config");
@@ -10,6 +10,18 @@ const path = require("path");
 
 const resolve = dir => path.resolve(__dirname, dir);
 module.exports = {
+    devServer:{
+        port:8080,
+        // proxy:{
+        //     "/api":{
+        //         target:"http://localhost:8000",
+        //         changeOrigin:true,
+        //         pathRewrite:{
+        //             "^/api":""
+        //         }
+        //     }
+        // }
+    },
     webpack: {
         alias: {
             "@": resolve("src"),
